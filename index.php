4cce277d2840163c1d8444b11e33af539f453fcc
Charles Willcockson <willcockson.charles@gmail.com>
2022-07-18 12:22:40 -0500

    starting blog page

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ get_header(); ?>
         while(have_posts()) {
           the_post(); ?>
           <div class="post-item">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
             <div class="metabox">
-              <p>Posted by Charles on 6.8.22 in News</p>
+              <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n-j-y'); ?> in <?php echo get_the_category_list(', '); ?></p>
             </div>
             <div class="generic-content">
               <?php the_excerpt(); ?>
